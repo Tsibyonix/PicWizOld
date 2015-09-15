81dbd91554145c8851ebd2f69491bf8a4f976b25
elearningpoint <shashankshekhar5823@gmail.com>
2015-09-15 14:49:45 +0530

    file upload has been saved but as uri is not working so havent coded latitude and longitude
    the server is configured to take latitude and longitude. use 'lat' and
    'lng ' to send the variable server will handle automatically.

--- app/src/main/java/com/core/project/picwiz/UploadPicture.java
+++ app/src/main/java/com/core/project/picwiz/UploadPicture.java
@@ -1,18 +1,24 @@
 package com.core.project.picwiz;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.database.Cursor;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,13 +27,17 @@ public class UploadPicture extends AppCompatActivity {
     Uri photoLocationURI;
     String getPhotoLocationPath;
     String TAG = "log";
+    int serverResponseCode = 0;
+    ProgressDialog dialog = null;
+    String upLoadServerUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_picture);
         ImageView imageView = (ImageView) findViewById(R.id.selectedImageView);
-
+        Button upLoad = (Button) findViewById(R.id.uploadbtn);
+        upLoadServerUri = "http://e-learningpoint.in/picwiz/get_image_picwiz.php";
         if(savedInstanceState == null) {
             Log.i(TAG, "Not a saved instance");
             Bundle extra = getIntent().getExtras();
@@ -46,6 +56,17 @@ public class UploadPicture extends AppCompatActivity {
 
         imageView.setImageURI(null);
         imageView.setImageURI(photoLocationURI);
+        upLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog = ProgressDialog.show(UploadPicture.this, "", "Uploading file...", true);
+
+                uploadFile(photoLocationURI.getPath());
+
+
+            }
+        });
     }
 
     public static Uri handleImageUri(Uri uri) {
@@ -74,4 +95,137 @@ public class UploadPicture extends AppCompatActivity {
             }
         }
     }
-}
+    public int uploadFile(String sourceFileUri) {
+
+
+        final String fileName = sourceFileUri;
+
+        HttpURLConnection connection = null;
+        DataOutputStream dos = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 5 * 1024 * 1024;
+        File sourceFile = new File(sourceFileUri);
+        if (!sourceFile.isFile()) {
+
+            dialog.dismiss();
+
+            Log.e("uploadFile", "Source File not exist :"
+            +photoLocation);
+
+
+
+            return 0;
+
+        }
+        else
+        {
+
+            try {
+
+                // open a URL connection to the Servlet
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                URL url = new URL(upLoadServerUri);
+
+                // Open a HTTP  connection to  the URL
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true); // Allow Inputs
+                connection.setDoOutput(true); // Allow Outputs
+                connection.setUseCaches(false); // Don't use a Cached Copy
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Connection", "Keep-Alive");
+                connection.setRequestProperty("ENCTYPE", "multipart/form-data");
+                connection.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                connection.setRequestProperty("uploaded_file", fileName);
+
+                dos = new DataOutputStream(connection.getOutputStream());
+
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name="+photoLocation+";filename="
+                                + fileName + "" + lineEnd);
+
+                        dos.writeBytes(lineEnd);
+
+                // create a buffer of  maximum size
+                bytesAvailable = fileInputStream.available();
+
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+
+                // read file and write it into form...
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0) {
+
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                }
+
+                // send multipart form data necesssary after file data...
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // Responses from the server (code and message)
+                serverResponseCode = connection.getResponseCode();
+                String serverResponseMessage = connection.getResponseMessage();
+
+                Log.i("uploadFile", "HTTP Response is : "
+                        + serverResponseMessage + ": " + serverResponseCode);
+
+                if(serverResponseCode == 200){
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+
+                            Toast.makeText(UploadPicture.this, "File Upload Complete.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+                //close the streams //
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+
+            } catch (MalformedURLException ex) {
+
+                dialog.dismiss();
+                ex.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+
+                        Toast.makeText(UploadPicture.this, "MalformedURLException",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            } catch (Exception e) {
+
+                dialog.dismiss();
+                e.printStackTrace();
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+
+                        Toast.makeText(UploadPicture.this, "Got Exception : see logcat ",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                //Log.e("Upload file to server Exception", "Exception : "+ e.getMessage(), e);
+            }
+            dialog.dismiss();
+            return serverResponseCode;
+
+        } // End else block
+    }}
+
+
