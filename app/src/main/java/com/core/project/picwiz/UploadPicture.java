94948336056014afc32657321a97ae474a844dd8
elearningpoint <shashankshekhar5823@gmail.com>
2015-09-18 09:07:04 +0530

    upload button added

--- app/src/main/java/com/core/project/picwiz/UploadPicture.java
+++ app/src/main/java/com/core/project/picwiz/UploadPicture.java
@@ -1,18 +1,15 @@
 package com.core.project.picwiz;
 
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.PointF;
-import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.Button;
@@ -20,14 +17,25 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class  UploadPicture extends AppCompatActivity {
     String photoLocation;
     Uri photoLocationURI;
     String photoLocationPath;
     String TAG = "log";
-
+    Button upload;
     float imageViewX, imageViewY;
-
+    public static Bitmap bitmap;
     TouchImageView imageView;
     TextView touch;
     TextView left;
@@ -46,7 +54,7 @@ public class  UploadPicture extends AppCompatActivity {
         setContentView(R.layout.activity_upload_picture);
         imageView = (TouchImageView) findViewById(R.id.customImageView);
         imageres = (TextView) findViewById(R.id.touch);
-
+        upload= (Button) findViewById(R.id.uploadbtn);
         //debug
         imageviewsize = (TextView) findViewById(R.id.imageviewsize);
         imageres  = (TextView) findViewById(R.id.resolution);
@@ -87,7 +95,7 @@ public class  UploadPicture extends AppCompatActivity {
                 imageViewX = (float) imageView.getMeasuredWidth();
                 imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
                 imageView.setImageURI(photoLocationURI);
-
+                bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
                 //scale(ScalingLogic.CROP);
             }
         });
@@ -95,7 +103,7 @@ public class  UploadPicture extends AppCompatActivity {
         crop_scale.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(currentScale.contains("crop")) {
+                if (currentScale.contains("crop")) {
                     imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
                     currentScale = "fit";
                 } else {
@@ -104,17 +112,20 @@ public class  UploadPicture extends AppCompatActivity {
                 }
             }
         });
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bitmap=null;
+
+                new UploadTask().execute();
+
+            }
+        });
+
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -130,6 +141,7 @@ public class  UploadPicture extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
     public String getRealPathFromURI(Uri contentUri) {
         String[] split;
         split = contentUri.toString().split("/");
@@ -142,8 +154,116 @@ public class  UploadPicture extends AppCompatActivity {
         }
         return finalPath;
     }
+    private class UploadTask extends AsyncTask<Bitmap, Void, Void> {
+
+        protected Void doInBackground(Bitmap... params) {
+            if (params == null)
+                return null;
+            setProgress(0); //initial progress of the progress bar
+
+             //storing the bitmap in a variable
+            ByteArrayOutputStream stream = new ByteArrayOutputStream(); //creating an object of byteArrayoutput stream
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream); // convert Bitmap to ByteArrayOutputStream
+            InputStream in = new ByteArrayInputStream(stream.toByteArray()); // convert ByteArrayOutputStream to ByteArrayInputStream
+            Log.d("image", "cant get the image ");
+            DefaultHttpClient httpclient = new DefaultHttpClient();
+            try {
+                HttpPost httppost = new HttpPost(
+                        "http://e-learningpoint.in/picwiz/uploadimage.php"); // server
+
+                MultipartEntity reqEntity = new MultipartEntity();
+                reqEntity.addPart("PicWiz",
+                        System.currentTimeMillis() + ".jpg", in);
+                httppost.setEntity(reqEntity);
+
+                Log.i(TAG, "request " + httppost.getRequestLine());
+                HttpResponse response = null;
+                try {
+                    response = httpclient.execute(httppost);
+                } catch (ClientProtocolException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                try {
+                    if (response != null)
+                        Log.i(TAG, "response " + response.getStatusLine().toString());
+                } finally {
+
+                }
+            } finally {
+
+            }
+
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            // TODO Auto-generated method stub
+            super.onProgressUpdate(values);
+        }
+
+
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // TODO Auto-generated method stub
+            super.onPostExecute(result);
+            Toast.makeText(UploadPicture.this, "uploaded", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+        Log.i(TAG, "onResume: " + this);
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        // TODO Auto-generated method stub
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // TODO Auto-generated method stub
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+    }
+
+    String mCurrentPhotoPath;
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+    File photoFile = null;
+
+
 }
-
-
-
-
