93ed23f702116f0d7b689d8478f8ddad09aac28e
Sanchit Samuel <sanchit.samuel@live.com>
2015-09-20 12:29:35 +0530

    Icons files.

--- app/src/main/java/com/core/project/picwiz/SplashScreen.java
+++ app/src/main/java/com/core/project/picwiz/SplashScreen.java
@@ -38,15 +38,15 @@ public class SplashScreen extends AppCompatActivity {
     String username;
     String email;
     private ProgressDialog pDialog;
-    public static String signup="http://picwiz.e-learningpoint.in/signup.php";
-    public static String login="http://picwiz.e-learningpoint.in/signup.php";
+    public static String signup = "http://picwiz.e-learningpoint.in/signup.php";
+    public static String login = "http://picwiz.e-learningpoint.in/signup.php";
     RequestQueue requestQueue;
     EditText usernameField;
     EditText emailField;
     Button accept;
     SharedPreferences settings;
     SharedPreferences.Editor editor;
-    StringRequest strReq=null;
+    StringRequest strReq = null;
 
 
     boolean isRun;
@@ -71,7 +71,7 @@ public class SplashScreen extends AppCompatActivity {
         settings = getSharedPreferences(SETTINGS_NAME, 0);
         editor = settings.edit();
 
-        if(username == null || email == null) {
+        if (username == null || email == null) {
             helloText.setText("Create New Account");
             usernameField.setVisibility(View.VISIBLE);
             emailField.setVisibility(View.VISIBLE);
@@ -102,90 +102,35 @@ public class SplashScreen extends AppCompatActivity {
                         pDialog.setMessage("Registering ...");
                         //pDialog.show();
 
-                            // Posting params to register url
-                            pDialog.setMessage("Uploading");
-                            Map<String, String> params = new HashMap<String, String>();
-                            params.put("tag", "register");
-                            params.put("name", username);
-                            params.put("email", email);
-                            editor.putString("username", usernameField.getText().toString());
-                            editor.putString("email", emailField.getText().toString());
-                            editor.commit();
-                            getUserInfo();
-
-
-
-                        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
-                                signup, new JSONObject(params),
-                                new com.android.volley.Response.Listener<JSONObject>() {
-
-                                    @Override
-                                    public void onResponse(JSONObject response) {
-                                        //Log.d("TAG", response.toString());
-                                        try {
-                                            //Toast.makeText(mContext, response.getString("message"), Toast.LENGTH_LONG).show();
-                                            Toast.makeText(SplashScreen.this, "Thank you for your post", Toast.LENGTH_LONG).show();
-
-                                            if (response.getBoolean("status")) {
-                                                pDialog.dismiss();
-                                                finish();
-                                            }
-                                        } catch (JSONException e) {
-                                            Log.e("TAG", e.toString());
-                                        }
-                                        pDialog.dismiss();
-                                    }
-                                }, new com.android.volley.Response.ErrorListener() {
-
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                //VolleyLog.d("TAG", "Error: " + error.getMessage());
-                                pDialog.dismiss();
-                                //if (isNetworkProblem(error)) {
-                                    Toast.makeText(SplashScreen.this, "Internet Problem", Toast.LENGTH_SHORT).show();
-
-                                //}
-                            }
-                        }) {
-
-                            @Override
-                            public String getBodyContentType() {
-                                return "application/json; charset=utf-8";
-                            }
-
-                          //  @Override
-                           // public Map<String, String> getHeaders() throws AuthFailureError {
-                          //      return getRequestHeaders();
-                            //}
-                        };
-
-                        jsonObjReq.setRetryPolicy(new DefaultRetryPolicy(8000, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-                        // Adding request to request queue
-                        SendOnline.getInstance().addToRequestQueue(jsonObjReq, tag_string_req);
-
+                        // Posting params to register url
+                        pDialog.setMessage("Uploading");
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("tag", "register");
+                        params.put("name", username);
+                        params.put("email", email);
+                        editor.putString("username", usernameField.getText().toString());
+                        editor.putString("email", emailField.getText().toString());
+                        editor.commit();
+                        getUserInfo();
                     }
 
 
+                    if (username == null || email == null)
 
+                    {
 
-                if(username==null||email==null)
+                    } else
 
-                {
-
+                    {
+                        Intent homeScreen = new Intent(SplashScreen.this, HomeScreen.class);
+                        homeScreen.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(homeScreen);
+                        finish();
+                    }
                 }
 
-                else
 
-                {
-                    Intent homeScreen = new Intent(SplashScreen.this, HomeScreen.class);
-                    homeScreen.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(homeScreen);
-                    finish();
-                }
-            }
-
-
-        });
+            });
         } else {
             Intent homeScreen = new Intent(this, HomeScreen.class);
             homeScreen.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -201,8 +146,5 @@ public class SplashScreen extends AppCompatActivity {
     }
 
 
-
-
-
-    }
+}
 
